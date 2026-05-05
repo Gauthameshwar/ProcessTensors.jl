826399348a8baf464cc19ca58c58d21e89125d4b
Gauthameshwar <gauthameshwar_s@mymail.sutd.edu.sg>
2026-05-05 19:40:05 +0800

    test: added comprehensive instruments tests
    - added test/systems/test_instruments.jl covering validation helpers, InstrumetnSeq behavior, leg-map coverage, a nd instrument_itensor semantics
    - asserted unsupported AbstractInstrument dispatch, currently throws MethodError. Will be extended in the future as the package develops
    - included systems/test_instruments.jl in test/runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,10 +12,16 @@ include(joinpath(@__DIR__, "liouvillian", "single_spin_analytical.jl"))
 
 include(joinpath(@__DIR__, "liouvillian", "liouvillian_methods.jl"))
 
+if Base.find_package("QuantumOptics") !== nothing
+    include(joinpath(@__DIR__, "liouvillian", "spinmpo_vs_qo.jl"))
+    include(joinpath(@__DIR__, "liouvillian", "bosonmpo_vs_qo.jl"))
+    nothing
+else
+    @info "Skipping QuantumOptics comparison tests because QuantumOptics is not installed."
+end
+
 include(joinpath(@__DIR__, "time_evolution", "tebd_validation.jl"))
 include(joinpath(@__DIR__, "time_evolution", "tebd_tfim_benchmarks.jl"))
 include(joinpath(@__DIR__, "time_evolution", "tdvp_tfim_benchmarks.jl"))
 
-include(joinpath(@__DIR__, "liouvillian", "spinmpo_vs_qo.jl"))
-
-include(joinpath(@__DIR__, "liouvillian", "bosonmpo_vs_qo.jl"))
+include(joinpath(@__DIR__, "systems", "test_instruments.jl"))
